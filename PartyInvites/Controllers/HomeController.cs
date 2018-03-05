f9a1ed338d63b8472c39a76674372219923609d6
telefoon13 <Mike0528>
2018-03-05 16:41:42 +0100

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        
+        public ViewResult Index()
+        /* public RedirectResult = Redirect naar andere url
+        * public String = Enkel string geven
+        * public HttpUnauthorizedResult = Force user to login
+        * Meer in Hoofdstuk 17
+        */
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Goede morgen deze morgen" : "Goede middag";
+
+            return View();
+        }
+
+
+        // GET : RsvpForm
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        //POST : RsvpForm
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO email sturen naar organisator
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Gelieven een naam in te vullen.")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Gelieven een email in te vullen.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Gelieven een geldig email te geven.")]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "Gelieven een telefoon nummer in te vullen.")]
+        public String Phone { get; set; }
+
+        [Required(ErrorMessage = "Gelieven een optie te kiezen.")]
+        public bool? WillAttend { get; set; }
+        //? na bool wil zeggen Nullable bool (kan true/false of null zijn )
+    }
+}
